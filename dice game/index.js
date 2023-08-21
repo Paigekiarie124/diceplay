29a9e2b55fde3e5fe8d3129b74560596dbd4faf0
Paigekiarie124 <123735229+Paigekiarie124@users.noreply.github.com>
2023-08-21 14:51:25 +0400

    Add files via upload

---
+++ dice game/index.js
@@ -0,0 +1,25 @@
+function randomDice (){
+
+}
+
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 *6;
+randomNumber1 = Math.floor(randomNumber1 + 1 );
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 *6;
+randomNumber2 = Math.floor(randomNumber2 + 1 );
+
+document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!"
+}
+
+else if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 1 wins !"
+    
+} else {
+    document.querySelector("h1").innerHTML = "player 2 wins !"
+}
